dc452d27d85e308ac8fbdfd11b42a5593480cc0a
FlavioAcuña <flavio.acuna.t@gmail.com>
2023-10-06 10:52:59 -0300

    new file:   Fundamentos.csproj 	new file:   Fundamentos.sln 	new file:   Program.cs 	new file:   bin/Debug/net7.0/Fundamentos.deps.json 	new file:   bin/Debug/net7.0/Fundamentos.dll 	new file:   bin/Debug/net7.0/Fundamentos.exe 	new file:   bin/Debug/net7.0/Fundamentos.pdb 	new file:   bin/Debug/net7.0/Fundamentos.runtimeconfig.json 	new file:   obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   obj/Debug/net7.0/Fundamentos.AssemblyInfo.cs 	new file:   obj/Debug/net7.0/Fundamentos.AssemblyInfoInputs.cache 	new file:   obj/Debug/net7.0/Fundamentos.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net7.0/Fundamentos.GlobalUsings.g.cs 	new file:   obj/Debug/net7.0/Fundamentos.assets.cache 	new file:   obj/Debug/net7.0/Fundamentos.csproj.AssemblyReference.cache 	new file:   obj/Debug/net7.0/Fundamentos.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net7.0/Fundamentos.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net7.0/Fundamentos.dll 	new file:   obj/Debug/net7.0/Fundamentos.genruntimeconfig.cache 	new file:   obj/Debug/net7.0/Fundamentos.pdb 	new file:   obj/Debug/net7.0/apphost.exe 	new file:   obj/Debug/net7.0/ref/Fundamentos.dll 	new file:   obj/Debug/net7.0/refint/Fundamentos.dll 	new file:   obj/Fundamentos.csproj.nuget.dgspec.json 	new file:   obj/Fundamentos.csproj.nuget.g.props 	new file:   obj/Fundamentos.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Comentario una linea
+/*Comentarios
+en 
+bloque
+*/
+
+short edad = 25;
+
+int numAlumnos = 1234567;
+
+long kilometros = 100000000000000;
+
+float calificacion = 9.8f;
+
+double PI = 3.1416;
+
+string nombre = "Alex";
+char letra = 'A';
+bool aprobado = true;
+
+string apellido = null;
+
+
+
+Console.WriteLine(edad);
+Console.WriteLine(numAlumnos);
+Console.WriteLine(kilometros);
+Console.WriteLine(calificacion);
+Console.WriteLine(PI);
+Console.WriteLine(nombre);
+Console.WriteLine(letra);
+Console.WriteLine(aprobado);
+Console.WriteLine(apellido);
